3a1d5dc159114d829ca3a7d8eef4e5b0f38d0b5e
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-29 15:17:00 -0300

    fix: Cria a busca por filme

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -1,26 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
-namespace RazorPagesMovie.Pages.Movies
+namespace RazorPagesMovie.Pages.Movies;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly Data.RazorPagesMovieContext _context;
+
+    public IndexModel(Data.RazorPagesMovieContext context)
     {
-        private readonly Data.RazorPagesMovieContext _context;
-
-        public IndexModel(Data.RazorPagesMovieContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Movie> Movie { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            if (_context.Movie != null)
-            {
-                Movie = await _context.Movie.ToListAsync();
-            }
-        }
+        _context = context;
     }
+
+    public IList<Movie> Movie { get;set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        var movies = from m in _context.Movie
+            select m;
+        if (!string.IsNullOrEmpty(SearchString))
+        {
+            movies = movies.Where(s => s.Title != null && s.Title.Contains(SearchString));
+        }
+
+        Movie = await movies.ToListAsync();
+    }
+
+    [BindProperty(SupportsGet = true)]
+    public string? SearchString {get; set;}
+
+    public SelectList? Genres {get; set;}
+
+    [BindProperty(SupportsGet = true)]
+    public string? MovieGenre {get; set;}
 }
