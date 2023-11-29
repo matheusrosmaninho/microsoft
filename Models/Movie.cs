96965c1a709b1ddc9b26d9be764100c061a0ac64
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-29 16:34:21 -0300

    fix: Add Rating

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,15 +5,17 @@ namespace RazorPagesMovie.Models;
 
 public class Movie {
     public int Id {get; set;}
-    
+
     public string? Title {get; set;}
-    
+
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate {get; set;}
-    
+
     public string? Genre {get; set;}
-    
+
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price {get; set;}
+
+    public string Rating {get; set;} = string.Empty;
 }
