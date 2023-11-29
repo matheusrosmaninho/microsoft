70c11183c89772ab27579717249b2aaf6d52b53f
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-29 15:47:32 -0300

    fix: Cria busca por genero

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -15,25 +15,36 @@ public class IndexModel : PageModel
         _context = context;
     }
 
-    public IList<Movie> Movie { get;set; } = default!;
+    public IList<Movie> Movie { get; set; } = default!;
 
     public async Task OnGetAsync()
     {
+        IQueryable<string> genreQuery = from m in _context.Movie
+                                        orderby m.Genre
+                                        select m.Genre;
+
         var movies = from m in _context.Movie
-            select m;
+                     select m;
+
         if (!string.IsNullOrEmpty(SearchString))
         {
             movies = movies.Where(s => s.Title != null && s.Title.Contains(SearchString));
         }
 
+        if (!string.IsNullOrEmpty(MovieGenre))
+        {
+            movies = movies.Where(x => x.Genre == MovieGenre);
+        }
+
+        Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
         Movie = await movies.ToListAsync();
     }
 
     [BindProperty(SupportsGet = true)]
-    public string? SearchString {get; set;}
+    public string? SearchString { get; set; }
 
-    public SelectList? Genres {get; set;}
+    public SelectList? Genres { get; set; }
 
     [BindProperty(SupportsGet = true)]
-    public string? MovieGenre {get; set;}
+    public string? MovieGenre { get; set; }
 }
