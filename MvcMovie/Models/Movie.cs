9da7523355c31d7a0e6fb95a47cd314bee20cefc
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-30 12:31:51 -0300

    Add data anotations no model para validar

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,16 +1,24 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
 public class Movie {
     public int Id {get; set;}
     
+    [Required]
     public string? Title {get; set;}
     
+    [Display(Name = "Release data")]
     [DataType((DataType.Date))]
+    [Required]
     public DateTime ReleaseDate {get; set;}
     
+    [Required]
     public string? Genre {get; set;}
     
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    [Range(1, 10000)]
     public decimal Price {get; set;}
 }
