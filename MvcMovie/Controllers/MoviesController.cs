311abdcd78f9e2e66673e92b6c6808f10a0ebd1c
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-12-01 09:30:51 -0300

    fix: Ajustes

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -20,12 +20,16 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
             if (_context.Movie == null)
             {
                 return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
             }
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                orderby m.Genre
+                select m.Genre;
 
             var movies = from m in _context.Movie
                 select m;
@@ -34,7 +38,25 @@ namespace MvcMovie.Controllers
             {
                 movies = movies.Where(s => s.Title!.Contains(searchString));
             }
-            return View(await movies.ToListAsync());
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVm = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            return View((IEnumerable<MovieGenreViewModel>)movieGenreVm);
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
         }
 
         // GET: Movies/Details/5
